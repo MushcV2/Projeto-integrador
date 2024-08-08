0820dca0b1f6fcc882342ab44b2382c3d520d549
MushcV2 <titi.c2006@gmail.com>
2024-08-08 11:50:42 -0300

    PC Changes
    Primeiro APP (TEMPLATE) adicionado
    Janela de aplicativos adicionado, podendo abrir e fechar ela

--- Assets/Project/Scripts/Item/PC/ComputerSystem.cs
+++ Assets/Project/Scripts/Item/PC/ComputerSystem.cs
@@ -8,7 +8,10 @@ public class ComputerSystem : MonoBehaviour
     [Header("Computer Variables")]
     [SerializeField] private PlayerController player;
     [SerializeField] private Button turnOnButton;
+    [SerializeField] private Button template;
     [SerializeField] private GameObject screenPanel;
+    [SerializeField] private GameObject appsPanel;
+    [SerializeField] private GameObject windownPanel;
     [SerializeField] private bool isOn;
 
     private void Start()
@@ -16,6 +19,11 @@ public class ComputerSystem : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
 
         turnOnButton.onClick.AddListener(TurnOnPc);
+        template.onClick.AddListener(OpenWindow);
+        windownPanel.GetComponentInChildren<Button>().onClick.AddListener(CloseWindow);
+
+        appsPanel.SetActive(false);
+        windownPanel.SetActive(false);
     }
 
     private void TurnOnPc()
@@ -23,12 +31,33 @@ public class ComputerSystem : MonoBehaviour
         if (!isOn) 
         {
             screenPanel.GetComponent<Animator>().SetTrigger("TurnOn");
+            Invoke(nameof(ActiveApps), 1.5f);
+
             isOn = true;
         }
         else
         {
             screenPanel.GetComponent<Animator>().SetTrigger("TurnOff");
+
+            windownPanel.SetActive(false);
+            appsPanel.SetActive(false);
+
             isOn = false;
         }
+    }
+
+    private void ActiveApps()
+    {
+        appsPanel.SetActive(true);
+    }
+
+    private void OpenWindow()
+    {
+        windownPanel.SetActive(true);
+    }
+
+    private void CloseWindow()
+    {
+        windownPanel.SetActive(false);
     }
 }
