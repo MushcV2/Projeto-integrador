e1b8da1e0f8282d8f7e9e1c3048208229eb16d42
MushcV2 <titi.c2006@gmail.com>
2024-08-09 11:44:58 -0300

    Day System changes

--- Assets/Project/Scripts/GameManager.cs
+++ Assets/Project/Scripts/GameManager.cs
@@ -13,15 +13,27 @@ public class GameManager : MonoBehaviour
     [SerializeField] private TMP_Text dayTXT;
     [SerializeField] private float timeElpase;
     [SerializeField] private float multiplier;
+    [SerializeField] private float seconds;
     [SerializeField] private int minutes;
     [SerializeField] private int days;
     [SerializeField] private bool cycleCompleted;
     [SerializeField] private bool dayCycleCompleted;
 
+
+    [Header("Star New Day Variables")]
+    [SerializeField] private GameObject dayPointsPanel;
+    [SerializeField] private Button startNextDay;
+
+
+
     private void Start()
     {
         StartCoroutine(LostSanity());
         dayTXT.text = "Days: " + days;
+
+        dayPointsPanel.SetActive(false);
+
+        startNextDay.onClick.AddListener(StarNewDay);
     }
 
     private void Update()
@@ -29,18 +41,14 @@ public class GameManager : MonoBehaviour
         timeElpase += Time.deltaTime * multiplier;
 
         UpdateClock(timeElpase);
-
-        if (minutes == 2 && !dayCycleCompleted)
-            DayCycle();
     }
 
     private void UpdateClock(float _time)
     {
+        if (minutes == 2 && !dayCycleCompleted) DayCycle();
+
         if (minutes >= 24)
-        {
             cycleCompleted = true;
-            dayCycleCompleted = false;
-        }
 
         if (cycleCompleted)
             minutes = -5;
@@ -48,18 +56,20 @@ public class GameManager : MonoBehaviour
             minutes = 19;
 
         minutes += Mathf.FloorToInt(_time / 60) % 60;
-        int _seconds = Mathf.FloorToInt(_time % 60);
+        seconds = Mathf.FloorToInt(_time % 60);
 
-        clockTXT.text = string.Format("{0:00}:{1:00}", minutes, _seconds);
+        clockTXT.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 
     private void DayCycle()
     {
-        days++;
-        minutes = 19;
+        print("Dia virou");
 
         dayCycleCompleted = true;
-        cycleCompleted = false;
+        multiplier = 0;
+        days++;
+
+        DisplayDayPoints();
 
         dayTXT.text = "Days: " + days;
     }
@@ -71,5 +81,24 @@ public class GameManager : MonoBehaviour
 
         yield return new WaitForSeconds(10);
         StartCoroutine(LostSanity());
+    }
+
+    private void DisplayDayPoints()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+
+        dayPointsPanel.SetActive(true);
+    }
+
+    private void StarNewDay()
+    {
+        dayPointsPanel.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        minutes = 19;
+        multiplier = 1;
     }
 }
