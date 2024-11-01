7748f2f650ec6af730bed6a1d4e68416c5665f5f
Thiago Correa Da Silva <ART0055333@aluno.firjansenaisesi.com.br>
2024-11-01 10:01:37 -0300

    UI Added

--- Assets/Project/Scripts/Player Scripts/SanityController.cs
+++ Assets/Project/Scripts/Player Scripts/SanityController.cs
@@ -7,15 +7,19 @@ public class SanityController : MonoBehaviour
 {
     [Header("Sanity Variables")]
     [SerializeField] private float maxSanity;
+    [SerializeField] private Sprite[] logoImages;
     public float currentSanity;
 
     [Header("Sanity UI")]
     [SerializeField] private Slider sanityBar;
+    [SerializeField] private Image logoImage;
 
     protected virtual void Start()
     {
         currentSanity = maxSanity;
         sanityBar.maxValue = maxSanity;
+
+        logoImage.sprite = logoImages[0];
 
         sanityBar.gameObject.SetActive(false);
     }
@@ -23,6 +27,10 @@ public class SanityController : MonoBehaviour
     protected virtual void Update()
     {
         sanityBar.value = Mathf.Lerp(sanityBar.value, currentSanity, 0.6f);
+
+        if (currentSanity < 75 && currentSanity >= 50) logoImage.sprite = logoImages[1];
+        else if (currentSanity < 50 && currentSanity >= 25) logoImage.sprite = logoImages[2];
+        else if (currentSanity < 25) logoImage.sprite = logoImages[3];
     }
 
     public void GainSanity(float _sanity)
