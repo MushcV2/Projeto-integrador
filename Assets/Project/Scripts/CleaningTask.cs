67b18485d5b649a265a0eff2b72368c17c6022d8
Thiago Correa Da Silva <ART0055333@aluno.firjansenaisesi.com.br>
2024-11-07 11:04:23 -0300

    Clean task 1.0

---
+++ Assets/Project/Scripts/CleaningTask.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CleaningTask : ObjectsInteract
+{
+    [SerializeField] private int stage;
+    [SerializeField] private bool already;
+    private bool havePlayer;
+    private bool canClean;
+    private bool finished;
+    private GameObject dustObject;
+
+    private void Update()
+    {
+        if (havePlayer && !finished)
+        {
+            Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            Physics.Raycast(_ray, out RaycastHit _hit, 100);
+
+            if (_hit.collider.CompareTag("Dust"))
+            {
+                if (dustObject == null) dustObject = _hit.collider.gameObject;
+                canClean = true;
+            }
+            else canClean = false;
+
+            if (Input.GetButtonDown("Fire1") && !already && canClean)
+            {
+                StartCoroutine(Clean());
+                player.canMove = false;
+            }
+
+            /*
+            if (Input.GetButtonDown("Fire1") && already && canClean)
+            {
+                StopCoroutine(Clean());
+
+                cam.GetComponent<CameraController>().stopFollowing = false;
+                already = false;
+                player.canMove = true;
+            }
+            */
+        }
+    }
+
+
+    private IEnumerator Clean()
+    {
+        already = true;
+
+        if (stage == 4)
+        {
+            cam.GetComponent<CameraController>().stopFollowing = false;
+
+            player.canMove = true;
+            finished = true;
+            dustObject.SetActive(false);
+
+            task.MissionCompleted(itemIndex);
+
+            yield break;
+        }
+
+        player.canMove = false;
+        cam.GetComponent<CameraController>().characterHead.localPosition = new Vector3(0f, cam.GetComponent<CameraController>().initialPos, 0f);
+        cam.GetComponent<CameraController>().stopFollowing = true;
+
+        Debug.Log("Mudou de estagio");
+
+        stage++;
+        yield return new WaitForSeconds(2f);
+
+        StartCoroutine(Clean());
+    }
+
+    public override void InteractFunction()
+    {
+        base.InteractFunction();
+
+        havePlayer = true;
+    }
+
+    public override void StopInteract()
+    {
+        base.StopInteract();
+
+        havePlayer = false;
+    }
+}
